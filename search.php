e85952941d22b56b4d977a6e160f99116d270b4d
Philip-2000 <1800013050@pku.edu.cn>
2021-02-18 19:58:25 +0800

    add.php structure

--- search.php
+++ search.php
@@ -2,27 +2,27 @@
 <?php
 $conn = new mysqli("localhost", "root", "", "fuga_songs");
 if($conn->connect_error) die("连接失败:" . $conn->connect_error);
-else echo "欢迎使用经年赋格歌曲查询系统
-<br />";
+else echo "欢迎使用经年赋格歌曲查询系统<br />";
 
-$result = $conn->query("SELECT * FROM style");
-$style_ret = "\n";
-if($result){
-    $style_ret = $style_ret . "<option value=\"0\"></option><option value=\"0\">不限</option>";
-	while($row = $result->fetch_assoc()) {
-		$style_ret = $style_ret . "<option value=\"" . $row['id'] . "\">" . $row['style'] . "</option>";
-	}
-}
-else echo "Error: " . "SELECT * FROM style" . $conn->error;
+$base = "<option value=\"0\"></option><option value=\"0\">不限</option>";
+$search = array("style","content","language","source", "period");
+$ret = array("","","","","","");
 
-$result = $conn->query("SELECT * FROM content");
-$content_ret = "<option value=\"0\"></option><option value=\"0\">不限</option>";
-if($result){
-	while($row = $result->fetch_assoc()) {
-		$content_ret = $content_ret . "<option value=\"" . $row['id'] . "\">" . $row['content'] . "</option>";
-	}
+for($i = 0; $i < 5; $i = $i+1){
+    $result = $conn->query("SELECT * FROM " . $search[$i]);
+    $ret[$i] = $base;
+    if($result){
+    	while($row = $result->fetch_assoc()) {
+    		$ret[$i] = $ret[$i] . "<option value=\"" . $row['id'] . "\">" . $row[$search[$i]] . "</option>";
+    	}
+    }
+else echo "Error: " . "SELECT * FROM " . $search[$i] . $conn->error;
 }
-else echo "Error: " . "SELECT * FROM content" . $conn->error;
+
+#$result = $conn->query("SELECT * FROM style");
+#$style_ret = "<option value=\"0\"></option><option value=\"0\">不限</option>";
+#if($result) while($row = $result->fetch_assoc()) $style_ret = $style_ret . "<option value=\"" . $row['id'] . "\">" . $row['style'] . "</option>";
+#else echo "Error: " . "SELECT * FROM style" . $conn->error;
 
 $conn->close();
 ?>
@@ -31,21 +31,26 @@ $conn->close();
     <meta charset="utf-8" />
     <meta name="author" content="Philip" />
     <meta name="keywords" content="Keywords" />
-    <title>test</title>
+    <title>查询歌曲</title>
 </head>
 <body>
     <button onclick="foo()">管理员</button>
     <textarea type="text" name="password" id="pwd" ></textarea>
-    <form action="PHPPage2.php" method="post">
-        <input type="submit" value="提交" />
+    <form action="result.php" method="post">
+        <input type="submit" value="提交" /><br />
         歌名：<input type="text" name="song" value="请输入歌名" id="111" /><br />
-        风格：<select id="style_select" innerHTML="<?php echo $style_ret; ?>"> </select> <br />
-        立意：<select id="content_select" innerHTML="<?php echo $content_ret; ?>"> </select> <br />
+        <br />
+        <br />
+        立意：<select name="content" innerHTML="<?php echo $ret[1]; ?>"> </select> 风格：<select name="style" innerHTML="<?php echo $ret[0]; ?>"> </select> <br />
+        语言：<select name="language" innerHTML="<?php echo $ret[2]; ?>"> </select> 来源：<select name="source" innerHTML="<?php echo $ret[3]; ?>"> </select> <br />
+        时期：<select name="period" innerHTML="<?php echo $ret[4]; ?>"> </select> <br />
+        <br />
+        <br />
         <input type="checkbox" name="accompany" value="accompany" />有伴奏<br />
-        <input type="radio" name="part" value="any" />不限<br />
-        <input type="radio" name="part" value="mix" />混声合唱<br />
-        <input type="radio" name="part" value="boy" />男声合唱<br />
+        <input type="radio" name="part" value="boy" />男声合唱
         <input type="radio" name="part" value="girl" />女声合唱<br />
+        <input type="radio" name="part" value="mix" />混声合唱
+        <input type="radio" name="part" value="any" checked="true"/>不限<br />
     </form>
 </body>
 </html>
